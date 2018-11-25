4870b1a8b97f00b4d2eeb3b3dd8a86023d25b8db
qiujiahong <qiujh@szfangle.com>
2018-11-26 07:21:55 +0800

    service_fetchdata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,40 +14,12 @@ import {CoursesService} from "./services/courses.service";
 export class AppComponent {
 
   courses$: Observable<Course[]>;
-  // courses;
 
-  constructor(private http: HttpClient,
-              private coursesService: CoursesService){
+  constructor(private coursesService: CoursesService){
 
   }
   ngOnInit(){
-
-    const params = new HttpParams()
-      .set("_page","1")
-      .set("_limit","3");
-    this.courses$ = this.http.get<Course[]>('/api/courses',{params});
-
-    //带参数
-    // const params = new HttpParams()
-    //   .set("_page","1")
-    //   .set("_limit","3");
-    // this.http.get('/api/courses',{params}).subscribe(
-    //   courses => this.courses = courses
-    // );
-
-    //无参数
-    // this.http.get('/api/courses').subscribe(
-    //   courses => this.courses = courses
-    // );
-
-    // this.http.get('http://localhost:3000/courses/1').subscribe(
-    //   val =>console.log(val)
-    // );
-
-    // this.http.get('/api/courses/1').subscribe(
-    //   val =>console.log(val)
-    // );
+    this.courses$ =  this.coursesService.loadCourses();
   }
-
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Course} from "../model/Course";
+import {Observable} from "rxjs";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams()
+      .set("_page", "1")
+      .set("_limit", "3");
+    return this.http.get<Course[]>('/api/courses', {params});
+  }
 }
