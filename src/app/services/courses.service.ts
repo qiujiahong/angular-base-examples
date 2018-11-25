f8289bb2cfea24923428b0fd66f64775becf5d89
qiujiahong <qiujh@szfangle.com>
2018-11-26 07:32:04 +0800

    put and headers

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Course} from "../model/Course";
 import {Observable} from "rxjs";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpHandler, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,11 @@ export class CoursesService {
       .set("_page", "1")
       .set("_limit", "3");
     return this.http.get<Course[]>('/api/courses', {params});
+  }
+
+  saveCourse(course: Course){
+    const headers = new HttpHeaders()
+      .set("X-Auth","userId");
+    return this.http.put('/api/courses/${course.id}',course,{headers});
   }
 }
