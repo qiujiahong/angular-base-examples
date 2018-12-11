c398f844fbfa5c4eb37073607cf0c5873d61e786
qiujiahong <qiujh@szfangle.com>
2018-12-11 17:49:56 +0800

    ViewChildren

---
+++ docs/ViewChildren.md
@@ -0,0 +1,72 @@
+# ViewChildren
+
+
+##  前言
+
+ViewChildren指令和ViewChild功能类似，只是该指令针对的是同一类型的一组组件。
+
+
+## 代码
+
+* 前端代码
+
+
+```html
+<div class="courses"  #container>
+
+  <app-course-card *ngFor="let course of courses"
+                   (courseSelected)="onCourseSelected($event)"
+                    [course]="course">
+  </app-course-card>
+
+</div>
+
+```
+
+
+* ts代码  
+```typescript
+export class AppComponent implements AfterViewInit{
+  courses = COURSES;
+
+  @ViewChildren(CourseCardComponent)
+  cards : QueryList<CourseCardComponent>;
+
+
+  onCourseSelected(course: Course){
+    console.log("hello");
+  }
+
+  constructor(private coursesService: CoursesService){
+  }
+  ngOnInit(){
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.cards)
+  }
+}
+```
+
+
+## QueryList.changes.subscribe
+
+* 如下代码，当cards值发生变化的时候，则会触发该函数执行
+
+
+```typescript
+this.cards.changes.subscribe(
+      cards => console.log(cards)
+    )
+```
+
+## 使用ViewChildren获取Dom对象
+
+
+```typescript
+  @ViewChildren(CourseCardComponent,{read: ElementRef})
+  cards : QueryList<ElementRef>;
+```
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Course} from "./model/Course";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {CoursesService} from "./services/courses.service";
 import { CourseCardComponent } from './course-card/course-card.component';
+import {COURSES} from "../db-data";
 
 
 
@@ -12,39 +13,42 @@ import { CourseCardComponent } from './course-card/course-card.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements AfterViewInit{
+export class AppComponent implements AfterViewInit{
+  courses = COURSES;
 
-  courses$: Observable<Course[]>;
+  @ViewChildren(CourseCardComponent)
+  cards : QueryList<CourseCardComponent>;
 
-  @ViewChild('container')
-  containerDiv: ElementRef;
-
-  // @ViewChild(CourseCardComponent)
-  // card: CourseCardComponent;
-
-  @ViewChild('cardRef1',{read: ElementRef})
-  card1: CourseCardComponent;
-  @ViewChild('cardRef2')
-  card2: CourseCardComponent;
+  // @ViewChildren(CourseCardComponent,{read: ElementRef})
+  // cards : QueryList<ElementRef>;
 
   onCourseSelected(course: Course){
-    console.log(this.card1);
-    
-    console.log(this.card2);
-
-    console.log(this.containerDiv)
+    console.log("hello");
   }
 
   constructor(private coursesService: CoursesService){
   }
   ngOnInit(){
-    this.courses$ =  this.coursesService.loadCourses();
+
   }
 
   ngAfterViewInit(): void {
-    console.log(this.card1);
-    console.log(this.card2);
-    console.log(this.containerDiv)
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    )
+    console.log(this.cards);
   }
 
+  onCourseEdit() {
+    this.courses.push(
+      {
+      id: 1,
+      description: "Angular core deep Dive",
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+      longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: 'INTERMEDIATE',
+      lessonsCount: 10
+      }
+    );
+  }
 }
