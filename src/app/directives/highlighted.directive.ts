dfc1ebe4cacfd835646e039eb2525466820d5ce6
qiujiahong <qiujh@szfangle.com>
2018-12-18 07:17:32 +0800

    attribute_directive 修改css样式

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import {Directive, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appHighlighted]'
@@ -7,6 +7,21 @@ export class HighlightedDirective {
 
   constructor() {
     console.log("directive created")
+  }
+
+  // @HostBinding('class.highlighted')
+  // get cssClasses(){
+  //   return true;
+  // }
+
+  // @HostBinding('className')
+  // get cssClasses(){
+  //   return "highlighted";
+  // }
+
+  @HostBinding('style.border')
+  get cssClasses(){
+    return "1px solid red";
   }
 
 }
